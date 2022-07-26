7b20c8168d0b2fefe8021cbe2d08d1bd541f8296
lia0wang <lia0wang@outlook.com>
2022-07-26 22:41:15 +0800

    first commit

---
+++ include/gdwg/graph.hpp
@@ -0,0 +1,11 @@
+#ifndef GDWG_GRAPH_HPP
+#define GDWG_GRAPH_HPP
+
+// This will not compile straight away
+namespace gdwg {
+	template<typename N, typename E>
+	class graph;
+
+} // namespace gdwg
+
+#endif // GDWG_GRAPH_HPP
